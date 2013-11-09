e29a10f4d73bdc135ab9ec007779645f3f574365
moyicat <moyicat@gmail.com>
2013-11-08 18:24:26 -0600

    Fix SQL Database

--- EasyPolling.sql
+++ EasyPolling.sql
@@ -2,10 +2,10 @@
 -- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
--- 主机: localhost
--- 生成日期: 2013 年 11 月 07 日 13:57
--- 服务器版本: 5.6.12
--- PHP 版本: 5.5.3
+-- Host: localhost
+-- Generation Time: Nov 09, 2013 at 01:24 AM
+-- Server version: 5.6.12
+-- PHP Version: 5.5.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- 数据库: `EasyPolling`
+-- Database: `EasyPolling`
 --
 CREATE DATABASE IF NOT EXISTS `EasyPolling` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `EasyPolling`;
@@ -25,7 +25,44 @@ USE `EasyPolling`;
 -- --------------------------------------------------------
 
 --
--- 表的结构 `Users`
+-- Table structure for table `Answer`
+--
+
+CREATE TABLE IF NOT EXISTS `Answer` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Poll_ID` int(11) DEFAULT NULL,
+  `Answer` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Poll`
+--
+
+CREATE TABLE IF NOT EXISTS `Poll` (
+  `ID` varchar(11) NOT NULL,
+  `Content` longtext,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Poll`
+--
+
+INSERT INTO `Poll` (`ID`, `Content`) VALUES
+('1kub', '{question: "What is your gender?", answer:["Male", "Female"]}'),
+('2lqb', '{question: "test", answer:["1"]}'),
+('5a0b', '{question: "Test", answer:["01", "124"]}'),
+('7m66', '{question: "Test AGain", answer:["A", "B", "C", "D"]}'),
+('cqgo', '{question: "Test", answer:["O1", "O2"]}'),
+('gyg2', '{question: "What&#39;s your favorite color?", answer:["Red", "Green"]}');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
 --
 
 CREATE TABLE IF NOT EXISTS `Users` (
@@ -33,17 +70,18 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `username` varchar(15) DEFAULT NULL,
   `password` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
--- 转存表中的数据 `Users`
+-- Dumping data for table `Users`
 --
 
 INSERT INTO `Users` (`ID`, `username`, `password`) VALUES
 (1, 'yuchaozh', '4262485789'),
 (2, 'yuchao', '123'),
 (3, '123', '321'),
-(4, '222', '222');
+(4, '222', '222'),
+(5, 'lalalala', '123');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
