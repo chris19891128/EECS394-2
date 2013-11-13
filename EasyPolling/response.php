aa79482cd7a2dfc2df6343deb8edcb81b26650a7
Zhou Yuchao <513629287@qq.com>
2013-11-13 17:35:18 -0600

    modify database password

--- EasyPolling/response.php
+++ EasyPolling/response.php
@@ -1,5 +1,5 @@
 <?php
-$mysql = new mysqli ( 'localhost', 'root', '', 'EasyPolling' ) or die ( 'Cannot connect to Database' );
+$mysql = new mysqli ( 'localhost', 'root', 'stu.fudan2013', 'EasyPolling' ) or die ( 'Cannot connect to Database' );
 $query = "INSERT INTO Answer (Poll_ID, Answer) values('$_POST[id]', '$_POST[choice]')";
 if (mysqli_query ( $mysql, $query )) {
 	echo "success";
