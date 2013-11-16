59e18c39f0d4c4757e9af156c0de92d2f383b080
chris19891128 <shichao@SHICHAOs-MacBook-Pro.local>
2013-11-16 17:18:58 -0600

    zend path

--- EasyPolling/email.php
+++ EasyPolling/email.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('include_path',ini_get('include_path').':lib/Zend');
 require_once 'lib/all_error.php';
 require_once 'lib/google-api-php-client/src/Google_Client.php';
 require_once 'lib/Zend/Mail/Protocol/Imap.php';
