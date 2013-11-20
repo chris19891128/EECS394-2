2923097795291b7276c16bb7b00eeeb32e9f53da
chris19891128 <shichao@SHICHAOs-MacBook-Pro.local>
2013-11-20 17:54:55 -0600

    login fixed

--- create-poll.php
+++ create-poll.php
@@ -35,7 +35,7 @@ if (! isset ( $_SESSION ['token'] )) {
 <?php
 if ($_POST) {
 	$mysql = new mysqli ( 'localhost', 'root', 'stu.fudan2013', 'EasyPolling' ) or die ( 'Cannot connect to Database' );
-	$query = "INSERT INTO Poll VALUES('" . $_POST ['id'] . "', '" . json_encode ( $_POST ['data'] ) . "', '" . json_encode ( $_POST ['recipient'] ) . "')";
+	$query = "INSERT INTO Poll VALUES('" . $_POST ['id'] . "', '" . json_encode ( $_POST ['data'] ) . "', '" . json_encode ( $_POST ['recipient'] ) . "', '" . $_POST ['me'] . "')";
 	if ($updateDb = $mysql->query ( $query ) or die ( $mysql->error )) {
 		send_email ( $_POST ['me'], $_POST ['pwd'], $_POST ['recipient'], $_POST ['id'] );
 		echo "Email send out success";
