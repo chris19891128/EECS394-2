ff3618fee8ac42ff3e3c0aae40f8dc75b08b7e37
chris19891128 <shichao@SHICHAOs-MacBook-Pro.local>
2013-11-20 16:45:11 -0600

    stat bug fixing

--- stat.php
+++ stat.php
@@ -21,8 +21,10 @@ require_once 'survey_db.php';
 $survey_id = $_GET ['id'];
 $survey = get_survey_by_id ( $_GET ['id'] );
 $stat = array ();
+$res = array ();
 for($i = 0; $i < count ( $survey ['answer'] ); $i ++) {
 	$stat [$i] = 0;
+	$res [$i] = array ();
 }
 
 $mysql = new mysqli ( 'localhost', 'root', 'stu.fudan2013', 'EasyPolling' ) or die ( 'Cannot connect to Database' );
@@ -32,6 +34,7 @@ $result = mysqli_query ( $mysql, $query );
 while ( $row = mysqli_fetch_array ( $result ) ) {
 	$index = intval ( $row ['Answer'] );
 	$stat [$index] ++;
+	$res [$i] [count ( $res [$i] )] = $row ['Respondant'];
 }
 
 $totalNumber = 0;
@@ -59,15 +62,34 @@ mysqli_close ( $mysql );
 			$choice = $survey ['answer'] [$i];
 			$count = $stat [$i];
 			if ($totalNumber > 0) {
-				$percent1 = $count / $totalNumber * 100;
-				$percent1 = $percent1 . '%';
-				$percent2 = (1 - $count / $totalNumber) * 100;
-				$percent2 = $percent2 . '%';
+				$percent1 = ($count / $totalNumber * 100) . '%';
+				$percent2 = ((1 - $count / $totalNumber) * 100) . '%';
 			} else {
 				$percent1 = '0%';
 				$percent2 = '100%';
 			}
-			echo "<tr><td style=\"width:100px\">$choice</td> <td style=\"width:\"><table width=\"100%\"><td style=\"width:" . "$percent1" . "\" bgcolor=\"DarkCyan\" height=\"20px\"></td><td style=\"width:" . "$percent2" . "\" height=\"20px\"></td></table></td><td style=\"width:100px\">$count</td> <td style=\"width:100px\">";
+			echo <<<END
+			<tr>
+				<td style=\"width:100px\">$choice</td> 
+				<td style=\"width:\">
+					<table width=\"100%\">
+						<td style=\"width:" . "$percent1" . "\" bgcolor=\"DarkCyan\" height=\"20px\"/>
+					<td style=\"width:" . "$percent2" . "\" height=\"20px\"/>
+					</table>
+				</td>
+				<td style=\"width:100px\">$count</td> 
+				<td style=\"width:100px\">";
+					<table width=\"100%\">
+END;
+			
+			foreach ( $res [$i] as $single_email ) {
+				echo "<tr><td>$single_email</td></tr>";
+			}
+			echo <<<END
+					</table>
+				</td>
+			</tr>
+END;
 		}
 		
 		?>
