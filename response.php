8d4739850e0af5c045085a80fd98e63480059b62
chris19891128 <shichao@SHICHAOs-MacBook-Pro.local>
2013-11-20 15:15:13 -0600

    bug 2: dup poll

--- response.php
+++ response.php
@@ -3,10 +3,10 @@
 // echo $_POST [choice];
 // echo $_POST [respondant];
 $mysql = new mysqli ( 'localhost', 'root', 'stu.fudan2013', 'EasyPolling' ) or die ( 'Cannot connect to Database' );
-$query = "SELECT COUNT(*) as total FROM Answer where id='" . $_POST [id] . "' and respondant='" . $_POST ['respondant'];
+$query = "SELECT COUNT(*) as total FROM Answer where Poll_ID='" . $_POST [id] . "' and Respondant='" . $_POST ['respondant'];
 $result = mysqli_query ( $mysql, $query );
 $data = mysql_fetch_assoc ( $result );
-echo $data;
+echo $data['total'] + ' hello\n';
 if ($data ['total'] > 0) {
 	echo "error";
 } else {
