b4475d1b973398e913baa81682dcb362bcab2e74
chris19891128 <shichao@SHICHAOs-MacBook-Pro.local>
2013-11-30 16:32:49 -0600

    edit poll finish

--- history-poll.php
+++ history-poll.php
@@ -32,7 +32,7 @@ if (! isset ( $_SESSION ['token'] )) {
 					$td1 = $('<td>' + polls[i]['survey']['question'] + '</td>');
 					$td2 = $('<td><a href="statp.php?id=' + polls[i]['id'] + '">See statistic result</a></td>');
 					$td3 = $('<td><a href="edit-poll.php?id=' + polls[i]['id'] + '">Add more recipients</a></td>');
-					$row = $('<tr></tr>').append($td1).append($td2).append($t3);
+					$row = $('<tr></tr>').append($td1).append($td2).append($td3);
 					$('#mt').append($row);
 				}
 				$('#root').show();
