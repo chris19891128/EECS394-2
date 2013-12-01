b9dea63c8a6b7677ebbe64d99d95ec22c0ee03ec
chris19891128 <shichao@SHICHAOs-MacBook-Pro.local>
2013-11-30 22:26:52 -0600

    test java api call

--- lib/good-email.php
+++ lib/good-email.php
@@ -1,10 +1,24 @@
 <?php
+set_include_path ( '..' );
+require_once 'lib/all_error.php';
+require_once 'lib/session.php';
+
+if (isset ( $_GET ['test'] )) {
+	session_start ();
+	$accessToken = getAccessToken ();
+	echo $accessToken;
+	send_good_email ( 'chris19891128@gmail.com', $accessToken, [ 
+			'chris1989apply@gmail.com',
+			'chaoshi2012@u.northwestern.edu' 
+	], '187e' );
+}
+
 /**
- * 
- * @param unknown $me
- * @param unknown $token
- * @param unknown $emails
- * @param unknown $survey_id
+ *
+ * @param unknown $me        	
+ * @param unknown $token        	
+ * @param unknown $emails        	
+ * @param unknown $survey_id        	
  */
 function send_good_email($me, $token, $emails, $survey_id) {
 	foreach ( $emails as $email ) {
@@ -16,10 +30,14 @@ function send_good_email($me, $token, $emails, $survey_id) {
 		$url = dirname ( dirname ( $cur_url ) ) . '/answer.php?id=' . $survey_id . '&responder=' . $email;
 		$content = "Please complete the poll through link " . $url;
 		
-		send_single_email ( $me, $token, $email, $subject, $content );
+		if (! send_single_email ( $me, $token, $email, $subject, $content )) {
+			return false;
+		}
 	}
+	return true;
 }
 /**
+ * This function contains harded coded functions
  *
  * @param unknown $me        	
  * @param unknown $token        	
@@ -27,5 +45,16 @@ function send_good_email($me, $token, $emails, $survey_id) {
  * @param unknown $survey_id        	
  */
 function send_single_email($me, $token, $email, $subject, $content) {
+	$JAVA_HOME = parse_ini_file ( './res/path.properties' )['java_home'];
+	$PATH = "$JAVA_HOME/bin:/usr/local/bin:/usr/bin:/bin";
+	putenv ( "JAVA_HOME=$JAVA_HOME" );
+	putenv ( "PATH=$PATH" );
 	
+	$exe = 'java -jar ./java/SMTP/out/send_email.jar ' . $me . ' ' . $token . ' ' . $email . ' "' . $subject . '" "' . $content . '" 2>&1';
+	exec ( $exe, $out );
+	if ($out [count ( $out ) - 1] == 'Successfully sent out') {
+		return true;
+	} else {
+		return false;
+	}
 }
