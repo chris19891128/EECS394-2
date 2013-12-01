929bd0ece389df348801bf34a09fe7394b7b134e
chris19891128 <shichao@SHICHAOs-MacBook-Pro.local>
2013-11-30 23:08:36 -0600

    test java api call

--- lib/good-email.php
+++ lib/good-email.php
@@ -6,7 +6,9 @@ session_start ();
 
 $accessToken = getAccessToken ();
 echo 'haha' . $accessToken;
-send_single_email ( 'chris19891128@gmail.com', $accessToken, 'chris1989apply@gmail.com', 'subject', '187e' );
+send_good_email ( 'chris19891128@gmail.com', $accessToken, array (
+		'chris1989apply@gmail.com' 
+), '187e' );
 
 /**
  *
@@ -15,43 +17,7 @@ send_single_email ( 'chris19891128@gmail.com', $accessToken, 'chris1989apply@gma
  * @param unknown $emails        	
  * @param unknown $survey_id        	
  */
-// function send_good_email($me, $token, $emails, $survey_id) {
-// $properties = parse_ini_file ( '../res/path.properties' );
-// if (! $properties) {
-// return false;
-// }
-
-// $java_home = $properties ['java_home'];
-// $path = "$java_home/bin:/usr/local/bin:/usr/bin:/bin";
-
-// putenv ( "JAVA_HOME=$java_home" );
-// putenv ( "PATH=$path" );
-
-// foreach ( $emails as $email ) {
-// echo $email . '::';
-// // subject
-// $subject = 'You have a new Poll';
-
-// // email content
-// $cur_url = 'http://' . $_SERVER ['HTTP_HOST'] . $_SERVER ['PHP_SELF'];
-// $url = dirname ( dirname ( $cur_url ) ) . '/answer.php?id=' . $survey_id . '&responder=' . $email;
-// $content = "Please complete the poll through link " . $url;
-
-// if (! send_single_email ( $me, $token, $email, $subject, $content )) {
-// return false;
-// }
-// }
-// return true;
-// }
-/**
- * This function contains harded coded functions
- *
- * @param unknown $me        	
- * @param unknown $token        	
- * @param unknown $email        	
- * @param unknown $survey_id        	
- */
-function send_single_email($me, $token, $email, $subject, $content) {
+function send_good_email($me, $token, $emails, $survey_id) {
 	$properties = parse_ini_file ( '../res/path.properties' );
 	if (! $properties) {
 		return false;
@@ -64,6 +30,33 @@ function send_single_email($me, $token, $email, $subject, $content) {
 	putenv ( "JAVA_HOME=$java_home" );
 	putenv ( "PATH=$path" );
 	
+	foreach ( $emails as $email ) {
+		echo $email . '::';
+		// subject
+		$subject = 'You have a new Poll';
+		
+		// email content
+		// $cur_url = 'http://' . $_SERVER ['HTTP_HOST'] . $_SERVER ['PHP_SELF'];
+		// $url = dirname ( dirname ( $cur_url ) ) . '/answer.php?id=' . $survey_id . '&responder=' . $email;
+		// $content = "Please complete the poll through link " . $url;
+		
+		$content = 'junk';
+		
+		if (! send_single_email ( $me, $token, $email, $subject, $content )) {
+			return false;
+		}
+	}
+	return true;
+}
+/**
+ * This function contains harded coded functions
+ *
+ * @param unknown $me        	
+ * @param unknown $token        	
+ * @param unknown $email        	
+ * @param unknown $survey_id        	
+ */
+function send_single_email($me, $token, $email, $subject, $content) {
 	$exe = 'java -jar ../java/SMTP/out/send_email.jar ' . $me . ' ' . $token . ' ' . $email . ' "' . $subject . '" "' . $content . '" 2>&1';
 	exec ( $exe, $out );
 	echo var_dump ( $out );
